3b95a9370c294daf45d86c4c0425189740853cb3
Kornel <kornel272@gmail.com>
2022-01-03 22:45:23 +0100

    projekt szacowania

--- glowny.py
+++ glowny.py
@@ -4,4 +4,4 @@ from zwracacz import aproksymujLicznik
 parametry_wejsciowe = [wynik_liczenia(0, 1, 2), wynik_liczenia(3, 4, 5)]
 wyniki = aproksymujLicznik(parametry_wejsciowe)
 for wynik in wyniki:
-    print (wynik_liczenia)
+    print (wynik.numer_podz, wynik.wspolczynnik, wynik.odczyt)
