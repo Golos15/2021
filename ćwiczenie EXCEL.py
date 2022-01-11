5401e64f52b72fdc0561aa212a276c667a1a585c
Golos15 <92723247+Golos15@users.noreply.github.com>
2022-01-11 12:40:57 +0100

    Delete ćwiczenie EXCEL.py

--- ćwiczenie EXCEL.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import load_workbook
-
-wb = load_workbook('E:\\ĆWICZENIE.XLSX')
-ws = wb['Arkusz1']
-
-for wiersz in ws.rows:
-    for komorka in wiersz:
-        print(komorka.value)
