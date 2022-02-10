fbced65ec208b18eef5aad8a7b5b0d14be041b2b
Golos15 <kornel272@gmail.com>
2022-02-10 16:50:21 +0100

    poprawa remisu

---
+++ Wyliczanie podatku - progi.py
@@ -0,0 +1,13 @@
+def tax (income):
+    incomeCap = {100000:0.40,30000:0.25,10000:0.10}
+    total = 0
+    for value in incomeCap:
+        if income >= value:
+            taxPercent = incomeCap.get(value)
+            sumToTaxFrom = income - value
+            total += (sumToTaxFrom * taxPercent)
+            income -= sumToTaxFrom
+    return int(total)
+
+print(tax(income=27500))
+
