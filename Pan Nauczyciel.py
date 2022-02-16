1da02656b8798bf720f1e1232ce4e9f5813e1fb0
Golos15 <kornel272@gmail.com>
2022-02-16 15:31:09 +0100

    poprawione wcięcia

--- Pan Nauczyciel.py
+++ Pan Nauczyciel.py
@@ -3,20 +3,21 @@ def dajOcene(ocena):
         ocena = int(ocena)
     except:
         return "Wpisz liczbę!"
-    if ocena >= 101:
-        return "Za wysoko!"
-    elif ocena > 90:
-        return "Uczeń dostał 5."
-    elif ocena >= 80:
-        return "Uczeń dostał 4."
-    elif ocena >= 70:
-        return "Uczeń dostał 3."
-    elif ocena >= 50:
-        return "Uczeń dostał 2."
-    elif ocena >= 0:
-        return "Uczeń dostał 1."
-    else:
-        return "Nie kłam!"
+    finally:
+        if ocena >= 101:
+            return "Za wysoko!"
+        elif ocena > 90:
+            return "Uczeń dostał 5."
+        elif ocena >= 80:
+            return "Uczeń dostał 4."
+        elif ocena >= 70:
+            return "Uczeń dostał 3."
+        elif ocena >= 50:
+            return "Uczeń dostał 2."
+        elif ocena >= 0:
+            return "Uczeń dostał 1."
+        else:
+            return "Nie kłam!"
 
 
 ocena = input("Ile punktów dostał uczeń?")
