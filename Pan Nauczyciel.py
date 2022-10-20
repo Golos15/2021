64a5b4f1b79abcf175fb927f6d417cfc73807e96
Golos15 <92723247+Golos15@users.noreply.github.com>
2022-10-20 15:32:45 +0200

    Update Pan Nauczyciel.py

--- Pan Nauczyciel.py
+++ Pan Nauczyciel.py
@@ -6,13 +6,13 @@ def dajOcene(ocena):
     finally:
         if ocena >= 101:
             return "Za wysoko!"
-        elif ocena > 90:
+        elif ocena > 80:
             return "Uczeń dostał 5."
-        elif ocena >= 80:
-            return "Uczeń dostał 4."
         elif ocena >= 70:
+            return "Uczeń dostał 4."
+        elif ocena >= 60:
             return "Uczeń dostał 3."
-        elif ocena >= 50:
+        elif ocena >= 40:
             return "Uczeń dostał 2."
         elif ocena >= 0:
             return "Uczeń dostał 1."
