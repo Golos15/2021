2c53ed627869297e3c058dcc44644427189f2bd3
Kornel <kornel272@gmail.com>
2022-10-31 21:31:57 +0100

    procentowy postęp liczenia do tysiąca

--- liczenie do tysiąca.py
+++ liczenie do tysiąca.py
@@ -5,4 +5,4 @@ do_ilu = 1000
 for x in range(0, do_ilu + 1):
     time.sleep(0.01)
     procent = round(float((x / do_ilu) * 100), 1)
-    print (procent, "%", end="\r", flush=True)
+    print ("Aktualny x = ", x, " (", procent, "%)", end="\r", flush=True, sep="")
