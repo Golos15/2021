530dc38436ffb8d397de248e92d09814aeda809c
Golos15 <92723247+Golos15@users.noreply.github.com>
2023-04-27 11:01:55 +0200

    Update Algorytm Luhna.py

--- Algorytm Luhna.py
+++ Algorytm Luhna.py
@@ -4,31 +4,26 @@ def algorytm_Luhna(nr_nieruch):
 
     suma1 = 0
     suma2 = 0
-    #suma_parzystej = 0
 
     for i, wartosc in enumerate(reversed(cyfry)):
         if i % 2 == 0:
             wartosc *= 2
             if len(str(wartosc)) > 1:
                 for cyfra in str(wartosc):
-                    #suma_parzystej += int(cyfra)
-                    #print(suma_parzystej)
                     suma1 += int(cyfra)
         else:
             suma1 += wartosc
 
     liczba_kontrolna = suma1 % 10
+    print("Liczba kontrolna: " + str(liczba_kontrolna))
 
     cyfry.append(liczba_kontrolna)
 
     for i, wartosc in enumerate(reversed(cyfry)):
         if i % 2 == 0:
             wartosc *= 2
-            print(wartosc)
             if len(str(wartosc)) > 1:
                 for cyfra in str(wartosc):
-                    #suma_parzystej += int(cyfra)
-                    #print(suma_parzystej)
                     suma2 += int(cyfra)
         else:
             suma2 += wartosc
@@ -37,7 +32,7 @@ def algorytm_Luhna(nr_nieruch):
     else:
         return False
 
-if algorytm_Luhna(999995) == True:
+if algorytm_Luhna(375069) == True:
     print("Jest okej")
 else:
     print("Coś nie tak")
